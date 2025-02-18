22a173edb093e2b9165d3c75f769e01e6d8380e1
Kupham9 <kupham9@gmail.com>
2025-02-18 13:41:28 -0600

    fix

--- join.py
+++ join.py
@@ -10,7 +10,7 @@ df_trends = pd.read_csv(TRENDSTF_FN)
 df_rgl = pd.read_csv(RGL_FN)
 df_etf2l = pd.read_csv(ETF2L_FN)
 
-
+print("joining dataframes...")
 merged_df = df_trends.merge(df_rgl, on="SteamID", how="outer")
 merged_df = merged_df.merge(df_etf2l, on="SteamID", how="outer")
 
@@ -21,5 +21,6 @@ merged_df = merged_df.merge(df_etf2l, on="SteamID", how="outer")
 merged_df.fillna(0, inplace=True) # filled with 0 to denote it being a null value
 
 
-
-merged_df.to_csv("players.csv")
+print("outputting to CSV...")
+merged_df.to_csv("players.csv", index=False)
+print("Done!")
